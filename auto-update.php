3b04ab3aea7d4527d7436a3f84f196c69c7b16cd
djmisha <misha.osinovskiy@gmail.com>
2018-02-10 08:38:04 -0800

    First commit
    added support for WordPress core, Themes, Plugins

---
+++ auto-update.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+  Plugin Name: The Auto Update  
+  Plugin URI: http://mynameismisha.com
+  Description: This must-use plugin will updated your WordPress site automatically. 
+  Version: 420 
+  Author: Misha Osinovskiy
+  Author URI: http://mynameismisha.com
+  Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
+ */
+
+
+// WordPress Core update all 
+
+// Enable major updates
+add_filter( 'allow_major_auto_core_updates', '__return_true' );         
+
+// Enable minor updates
+
+add_filter( 'allow_minor_auto_core_updates', '__return_true' );         
+// Enable development updates 
+
+add_filter( 'allow_dev_auto_core_updates', '__return_true' );  
+
+// Auto Update All Plugins 
+add_filter( 'auto_update_plugin', '__return_true' );
+
+// Auto Update All Themes 
+add_filter( 'auto_update_theme', '__return_true' );
+
+// Make a note in the head that plugin is loaded  
+
+add_action('wp_head','header_hook');
+
+function header_hook() {
+	echo '<!-- Auto updates are ON -->';
+}
